a21ee919ed3c5aa4c89e7aef3645d0615c78d95f
MIGUEL PEREIRA CANDIDO <miguel.candido@a.ucb.br>
2025-02-27 11:03:46 -0300

    exercício variáveis e operações

---
+++ ATIVIDADE 1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num_A = 10;
+let num_B = 20;
+
+let soma = (num_A + num_B);
+let subtracao = (num_A - num_B);
+let multi = (num_A / num_B);
+let divisao = (num_A * num_B);
+
+console.log (`Resultado da Soma e: ${soma}`)
+console.log (`Resultado da Soma e: ${subtracao}`)
+console.log (`Resultado da Soma e: ${multi}`)
+console.log (`Resultado da Soma e: ${divisao}`)
